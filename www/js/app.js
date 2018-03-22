871e59294918043b21b27ee2db57de712c77c349
LUCAS ABRILE BRAGA <lucas.abraga@senacsp.edu.br>
2018-03-21 22:22:00 -0300

    21/03/2018

--- www/js/app.js
+++ www/js/app.js
@@ -41,7 +41,7 @@ var mainView = app.views.create('.view-main', {
 });
 
 // Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
+$$('#my-login-screen .SignUp').on('click', function () {
   var username = $$('#my-login-screen [name="username"]').val();
   var password = $$('#my-login-screen [name="password"]').val();
 
@@ -50,4 +50,97 @@ $$('#my-login-screen .login-button').on('click', function () {
 
   // Alert username and password
   app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+
+firebase
+    .auth()
+    .createUserWithEmailAndPassword(username,password)
+    .then( function () {
+      app.dialog.alert('Bem vindo: ' + username);
+      this.$$('.toolbar-inner').text('Bem vindo: ' + username);
+
+    })
+
+    .catch( function(error){
+      console.error(error.code)
+      console.error(error.message)
+      app.dialog.alert('Falha ao cadastrar, verifique o erro no console');
+      // this.$$('.toobar-inner').text('Bem vindo ' + username);
+    })
+
+    app.loginScreen.close('#my-login-screen');
+  });
+
+
+$$('#my-login-screen .SignUp').on('click', function () {
+  var username = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  // app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+  
+  firebase
+    .auth()
+    .SignInUserWithEmailAndPassword(username,password)
+    .then( function () {
+      app.dialog.alert('Bem vindo: ' + username);
+      this.$$('.toolbar-inner').text('Bem vindo: ' + username + 'logado !');
+      $$('.logoff').show();
+      $$('.login-screen-open').hide();
+      $$('input#username').val('');
+      $$('input#password').val('');
+    })
+
+    .catch( function(error){
+      console.error(error.code)
+      console.error(error.message)
+      if (error.code =="auth/invalid-username"){
+        app.dialog.alert('Email inválido no seu formato !!!');
+      }
+      app.dialog.alert('Falha ao cadastrar, verifique o erro no console');
+      // this.$$('.toobar-inner').text('Bem vindo ' + username);
+    })
+
+    app.loginScreen.close('#my-login-screen');
 });
+
+      $$('#my-login-screen .SignOut').on('click', function () {
+        app.loginScreen.close('#my-login-screen');
+        $$('input#username').val('');
+        $$('input#password').val('');
+        firebase
+          .auth()
+          .signOut()
+          .then(function () {
+            this.$$('toolbar-inner').text('Usuario não autenticado');
+            app.dialog.alert('Usuario não atenticado');
+            app.loginScreen.close('#my-login-screen');
+            $$('logoff').hide();
+            $$('login-screen-open').show();
+          }, function(error) {
+            console.error(error)
+        })
+      });
+
+   $$('#my-login-screen .login-screen-close').on('click', function () {
+     $$('input#username').val('');
+     $$('input#password').val('');
+   })   
+
+   $$('logoff').on('click', function () {
+     firebase
+     .auth()
+     .signOut()
+     .then(function () {
+       this.$$('toolbar-inner').text('Usuario não atenticado');
+       app.dialog.alert('Usuario não autenticado');
+       $$('input#username').val('');
+       $$('input#password').val('');
+       $$('.logoff').hide();
+       $$('.login-screen-open').show();
+     }, function(error){
+       console.error(error)
+  })
+})
